b692664bf2a4c4041df8774b2c81276a43697ca0
jhuber <jhuber@grey-13.csc.kth.se>
2017-12-06 08:59:03 +0100

    Initialized View

--- src/swingBrowser/View.java
+++ src/swingBrowser/View.java
@@ -1,5 +1,23 @@
 package swingBrowser;
 
-public class View {
-    
+import javax.swing.JButton;
+import javax.swing.*;
+
+public class View extends JPanel {
+
+    public final JButton back;
+    public final JButton forward;
+    public final JButton go;
+    public final JButton history;
+    public final JButton close;
+    protected JTextField addressBar;
+
+    public View() {
+        back = new JButton();
+        forward = new JButton();
+        go = new JButton();
+        history = new JButton();
+        close = new JButton();
+        addressBar = new JTextField();
+    }
 }
