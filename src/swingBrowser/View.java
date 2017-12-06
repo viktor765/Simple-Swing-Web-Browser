48673470610490b6614f446b28bcf3f684d61a7b
jhuber <jhuber@grey-13.csc.kth.se>
2017-12-06 09:44:35 +0100

    added components to panel

--- src/swingBrowser/View.java
+++ src/swingBrowser/View.java
@@ -1,22 +1,52 @@
 package swingBrowser;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class View extends JPanel {
-
+    
+    public final JFrame myFrame;
     public final JButton back;
     public final JButton forward;
     public final JButton go;
     public final JButton history;
     public final JButton close;
-    protected JTextField addressBar;
+    public final JTextField addressBar;
+    public JEditorPane editorPane;
+    private final Model model;
+    
+    private static final int X_SIZE = 700;  
+    private static final int Y_SIZE = 500;
 
     public View(Model model) {
-        back = new JButton();
-        forward = new JButton();
-        go = new JButton();
-        history = new JButton();
-        close = new JButton();
-        addressBar = new JTextField();
+        this.model = model;
+        
+        myFrame = new JFrame();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        editorPane = new JEditorPane();
+        editorPane.setEditable(false);
+        
+        setPreferredSize(new Dimension(X_SIZE, Y_SIZE));
+        
+        back = new JButton("Back");
+        forward = new JButton("Forward");
+        go = new JButton("Go");
+        history = new JButton("History");
+        close = new JButton("Close");
+        addressBar = new JTextField(20);
+        
+        myFrame.add(this);
+
+        this.add(back);
+        this.add(forward);
+        this.add(addressBar);
+        this.add(go);
+        this.add(history);
+        this.add(close);
+        
+        
+        myFrame.pack();
+        myFrame.setVisible(true); 
     }
 }
