f1c6ae2a90c330387b08bbbe5586bfbca11c961f
Viktor Nilsson <vikn@kth.se>
2017-12-06 09:29:11 +0100

    Added dummy events for buttons

--- src/swingBrowser/Controller.java
+++ src/swingBrowser/Controller.java
@@ -1,5 +1,56 @@
 package swingBrowser;
 
+import java.awt.event.*;
+
 public class Controller {
+    private final Model model;
+    private final View view;
     
+    public Controller (Model model, View view) {
+        System.out.println("Hello world!");
+        
+        this.model = model;
+        this.view = view;
+        
+        view.go.addActionListener(goListener);
+        view.back.addActionListener(backListener);
+        view.forward.addActionListener(forwardListener);
+        view.history.addActionListener(historyListener);
+        view.close.addActionListener(closeListener);
+    }
+    
+    private final ActionListener goListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Go pushed.");
+        }
+    };
+    
+    private final ActionListener backListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Back pushed.");
+        }
+    };
+    
+    private final ActionListener forwardListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Forward pushed.");
+        }
+    };
+    
+    private final ActionListener historyListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("History pushed.");
+        }
+    };
+    
+    private final ActionListener closeListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Close pushed.");
+        }
+    };
 }
--- src/swingBrowser/MyFrame.java
+++ src/swingBrowser/MyFrame.java
@@ -2,6 +2,7 @@ package swingBrowser;
 
 public class MyFrame {
     public static void main(String[] args) {
-        
+        Model model = new Model();
+        new Controller(model, new View(model));
     }
 }
