d6e1aa896abafad0c77463d4ae9aca08fadfb9f0
Viktor Nilsson <vikn@kth.se>
2017-12-06 08:35:34 +0100

    Initial commit

---
+++ src/swingBrowser/Controller.java
@@ -0,0 +1,5 @@
+package swingBrowser;
+
+public class Controller {
+    
+}
---
+++ src/swingBrowser/MyFrame.java
@@ -0,0 +1,7 @@
+package swingBrowser;
+
+public class MyFrame {
+    public static void main(String[] args) {
+        
+    }
+}
